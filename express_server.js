b7bcafc9081d4d8f13cb6da46f8c0834fe66b499
PaolaP <paolaperez8@gmail.com>
2021-11-25 16:17:45 -0500

    lecture notes, add morgan, add style sheet

--- express_server.js
+++ express_server.js
@@ -1,14 +1,17 @@
 /** @format */
 
 const express = require("express");
+//const morgan = require("morgan");
 const app = express();
 const PORT = 8080; // default port 8080
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //this is how we are processing views
 
-app.use(morgan("dev"));
+//app.use(morgan("dev"));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"));
 
 const urlDatabase = {
 	b2xVn2: "http://www.lighthouselabs.ca",
@@ -39,12 +42,13 @@ app.get("/hello", (req, res) => {
 });
 app.get("/urls", (req, res) => {
 	const templateVars = { urls: urlDatabase };
-	res.render("urls_index", templateVars);
+	res.render("urls_index", templateVars); //first argument is the file/template and second is the object we want to use
 });
 app.get("/urls/new", (req, res) => {
 	res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
+	// : for dynamic purposes to req.params
 	console.log(req.params.shortURL);
 	const shortURL = req.params.shortURL;
 	const longURL = urlDatabase[shortURL];
@@ -54,20 +58,21 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
 	const longURL = urlDatabase[req.params.shortURL];
-	res.redirect(longURL);
+	return res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
 	const shortURL = generateRandomString(6);
 	urlDatabase[shortURL] = req.body.longURL;
 	console.log(urlDatabase); // Log the POST request body to the console
-	res.redirect("/urls${shortURL}"); // Respond with 'Ok' (we will replace this)
+	return res.redirect("/urls${shortURL}"); // Respond with 'Ok' (we will replace this)
 });
 //how to delete? test
-app.post("/urls/<%=url%>/delete", (req, res) => {
+app.post("/urls/:url/delete", (req, res) => {
+	const shortURL = request.params.url;
 	console.log(urlDatabase[shortURL]);
 	delete urlDatabase[shortURL];
-	res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
+	return res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
 });
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}!`);
