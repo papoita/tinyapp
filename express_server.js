1b78236a53afa5761cf84749bcd1d97b98b22afb
PaolaP <paolaperez8@gmail.com>
2021-12-04 00:16:22 -0500

    testing sessions no cookies - doesn't work

--- express_server.js
+++ express_server.js
@@ -3,15 +3,21 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcrypt");
 
 const PORT = 8080; // default port 8080
 const app = express();
 
 app.use(morgan("short"));
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
-const bcrypt = require("bcrypt");
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["key"],
+		maxAge: 24 * 60 * 60 * 1000, // 24 hours
+	})
+);
 
 app.set("view engine", "ejs"); //this is how we are processing views
 
@@ -55,14 +61,15 @@ const findUserByEmail = (email) => {
 
 //helper function shoul return an object {shortUrl: longURL} to match our previous database forms
 const urlForUsers = function (userID, urlDatabase) {
-	const newUrlDatabase = {};
+	const userUrlDatabase = {};
 
 	for (let shortURL in urlDatabase) {
 		if (userID === urlDatabase[shortURL].userID) {
-			newUrlDatabase[shortURL] = urlDatabase[shortURL];
+			userUrlDatabase[shortURL] = urlDatabase[shortURL];
 		}
 	}
-	return newUrlDatabase;
+	return userUrlDatabase;
+	//return newUrlDatabase;
 };
 
 function generateRandomString() {
@@ -77,35 +84,49 @@ function generateRandomString() {
 	return result;
 }
 
-app.get("/", (req, res) => {
-	//listen to get request / localhost8080
-	res.send("Hello!");
+app.get("/urls.json", (req, res) => {
+	res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-	res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/register", (req, res) => {
+	const user = null;
+	res.render("urls_registration", { user, error: null });
 });
-//read main page
-app.get("/urls", (req, res) => {
-	const id = req.cookies.user_id;
-	//	console.log(id);
-	const user = users[id];
-	//console.log(urlForUsers(id, urlDatabase));
-	const templateVars = {
-		urls: urlForUsers(id, urlDatabase),
-		user,
-	};
-	if (!user) {
-		return res.status(401).send("To view your tinyUrls please login first");
+
+//TODO revise if database has to change, correct way to pass in the crypto
+
+app.post("/register", (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const hashedPassword = bcrypt.hashSync(password, 10);
+	if (!email || !password) {
+		res.status(401);
+		return res.render("urls_registration", {
+			user: null,
+			error: "Try again: enter a valid email and password",
+		});
 	}
-	res.render("urls_index", templateVars);
+	const userExists = findUserByEmail(email);
+	if (userExists) {
+		res.status(403);
+		return res.render("urls_registration", {
+			user: null,
+			error: "Try again: email already in use",
+		});
+	}
+	const id = generateRandomString(6);
+	users[id] = { id, email, hashedPassword }; //check this!!
+	console.log("new objwect hashed", users[id]);
+	//res.cookie("user_id", id);
+	req.session.user_id = id;
+	return res.redirect("/urls");
 });
-//urlDatabase { shortURL, shortURL.longURL, shortURL.userID}
 
 app.get("/login", (req, res) => {
 	const user = null;
 	res.render("urls_login", { user, error: null });
 });
+
 //TODO comapre passwords
 app.post("/login", (req, res) => {
 	const email = req.body.email;
@@ -142,43 +163,43 @@ app.post("/logout", (req, res) => {
 	return res.redirect("/urls");
 });
 
-app.get("/register", (req, res) => {
-	const user = null;
-	res.render("urls_registration", { user, error: null });
-});
-//TODO revise if database has to change, correct way to pass in the crypto
-app.post("/register", (req, res) => {
-	const email = req.body.email;
-	const password = req.body.password;
-	const hashedPassword = bcrypt.hashSync(password, 10);
-	if (!email || !password) {
-		res.status(401);
-		return res.render("urls_registration", {
-			user: null,
-			error: "Try again: enter a valid email and password",
-		});
+//read main page
+//TODO how to include here my helper function, how does it work if my arguments an dparameters are called diff?)
+app.get("/urls", (req, res) => {
+	const id = req.session.user_id;
+	//	console.log(id);
+	const user = users[id];
+	//console.log(urlForUsers(id, urlDatabase));
+	const templateVars = {
+		urls: urlForUsers(id, urlDatabase),
+		user,
+	};
+	if (!user) {
+		return res.status(401).send("To view your tinyUrls please login first");
 	}
-	const userExists = findUserByEmail(email);
-	if (userExists) {
-		res.status(403);
-		return res.render("urls_registration", {
-			user: null,
-			error: "Try again: email already in use",
-		});
-	}
-	const id = generateRandomString(6);
-	users[id] = { id, email, hashedPassword }; //check this!!
-	console.log("new objwect hashed", users[id]);
-	res.cookie("user_id", id);
-	return res.redirect("/urls");
+	res.render("urls_index", templateVars);
 });
+//Trying to use the helper functions
+// app.get("/urls", (req, res) => {
+// 	const userUrlDatabase = urlForUsers(req.session.user_id, urlDatabase);
+
+// 	const templateVars = {
+// 		urls: userUrlDatabase,
+// 		user: users[req.session.user_id],
+// 	};
+// 	// if (!users[req.session.user_id]) {
+// 	// 	return res.status(401).send("To view your tinyUrls please login first");
+// 	// }
+// 	res.render("urls_index", templateVars);
+// });
+//urlDatabase { shortURL, shortURL.longURL, shortURL.userID}
 
 //create short url
 app.post("/urls", (req, res) => {
 	const shortURL = generateRandomString(6);
 	urlDatabase[shortURL] = {
 		longURL: req.body.longURL,
-		userID: req.cookies.user_id,
+		userID: req.session.user_id,
 	};
 	//console.log(urlDatabase); // Log the POST request body to the console
 	return res.redirect(`/urls/${shortURL}`);
@@ -186,7 +207,7 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
 	//const userExists = findUserByEmail();
-	const id = req.cookies.user_id;
+	const id = req.session.user_id;
 	const user = users[id];
 	if (!user) {
 		return res.redirect("/login");
@@ -194,17 +215,40 @@ app.get("/urls/new", (req, res) => {
 
 	res.render("urls_new", { user });
 });
+//truing to use the templateVars
+// app.get("/urls/new", (req, res) => {
+// 	//const userExists = findUserByEmail();
+
+// 	if (!users[req.session.user_id]) {
+// 		return res.redirect("/login");
+// 	}
+
+// 	const templateVars = {
+// 		urls: urlDatabase,
+// 		user: users[req.session.user_id],
+// 	};
+
+// 	res.render("urls_new", { templateVars });
+// });
 
 app.get("/urls/:shortURL", (req, res) => {
-	const id = req.cookies.user_id;
+	//another way of writting template vars
+	const shortURL = req.params.shortURL;
+	const longURL = urlDatabase[shortURL].longURL;
+	const id = req.session.user_id;
 	const user = users[id];
+	const templateVars = { shortURL, longURL, user };
+	return res.render("/urls_show", templateVars);
+});
 
+app.post("/urls/:shortURL", (req, res) => {
 	const shortURL = req.params.shortURL;
 
-	const longURL = urlDatabase[shortURL].longURL;
-
-	const templateVars = { shortURL, longURL, user };
-	return res.render("urls_show", templateVars);
+	urlDatabase[shortURL] = {
+		longURL: req.body.longURL,
+		userID: req.session.user_id,
+	};
+	return res.redirect("/urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -214,38 +258,24 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
-	const id = req.cookies.user_id;
+	const id = req.session.user_id;
 	const shortURL = req.params.shortURL;
 	if ((urlDatabase[shortURL].userID = id)) {
 		urlDatabase[shortURL] = {
 			longURL: req.body.longURL,
-			userID: req.cookies.user_id,
+			userID: req.session.user_id,
 		};
 	}
 	return res.redirect("/urls/:shortURL");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-	const id = req.cookies.user_id;
+	const id = req.session.user_id;
 	const shortURL = req.params.shortURL;
 	if ((urlDatabase[shortURL].userID = id)) {
 		delete urlDatabase[shortURL];
 	}
 	return res.redirect("/urls");
-});
-
-app.post("/urls/:shortURL", (req, res) => {
-	const shortURL = req.params.shortURL;
-
-	urlDatabase[shortURL] = {
-		longURL: req.body.longURL,
-		userID: req.cookies.user_id,
-	};
-	return res.redirect("/urls");
-});
-
-app.get("/urls.json", (req, res) => {
-	res.json(urlDatabase);
 });
 
 //catch all in case the page is not found
